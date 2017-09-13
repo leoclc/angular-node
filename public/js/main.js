7701962a05cc1cbc1f5048a647ee4fa4df98106e
Leonardo Castanheira <leoclc@hotmail.com>
2017-09-13 11:44:05 +0100

    Routes and html 5 mode

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
 angular.module('studying', ['directives','ngAnimate','ngRoute'])
-.config(function($routeProvider){
+.config(function($routeProvider, $locationProvider){
+	
+	$locationProvider.html5Mode(true);
+
 	$routeProvider.when('/fotos', {
 		templateUrl: 'partials/main.html',
 		controller: 'FotosController'
 	});
+
+	$routeProvider.otherwise({redirectTo: '/fotos'});
 });
